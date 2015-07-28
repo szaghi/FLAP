ab25d3fa4f3b5c216a19402d094cd00af57a424f
Stefano Zaghi <stefano.zaghi@gmail.com>
2015-07-28 08:02:38 +0000

    Travis CI autocommit from travis build 64

--- src/Test_Driver.f90
+++ src/Test_Driver.f90
@@ -1,39 +1,219 @@
-!< Testing program for IR_Precision, Pure Fortran (2003+) library for ensuring codes portability
+!< A testing program for FLAP, Fortran command Line Arguments Parser for poor people
 program Test_Driver
-!-----------------------------------------------------------------------------------------------------------------------------------
-!< Testing program for IR_Precision, Pure Fortran (2003+) library for ensuring codes portability
+!< A testing program for FLAP, Fortran command Line Arguments Parser for poor people
 !<
-!<### Usage
-!<```bash
-!< ./Test_Driver
-!<```
+!<### Compile
+!< See [compile instructions](https://github.com/szaghi/FLAP/wiki/Download-compile).
+!<
+!<###Usage Compile
+!< See [usage instructions](https://github.com/szaghi/FLAP/wiki/Testing-Programs).
 !-----------------------------------------------------------------------------------------------------------------------------------
-USE IR_Precision
-USE, intrinsic:: ISO_FORTRAN_ENV, only: stdout=>OUTPUT_UNIT
+USE IR_Precision                                                        ! Integers and reals precision definition.
+USE Data_Type_Command_Line_Interface, only: Type_Command_Line_Interface ! Definition of Type_Command_Line_Interface.
 !-----------------------------------------------------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------------------------------------------------
 implicit none
+type(Type_Command_Line_Interface) :: cli          !< Command Line Interface (CLI).
+character(99)                     :: sval         !< String value.
+real(R8P)                         :: rval         !< Real value.
+real(R8P)                         :: prval        !< Positional real value.
+integer(I4P)                      :: ival         !< Integer value.
+integer(I4P)                      :: ieval        !< Exclusive integer value.
+integer(I4P)                      :: envi         !< Environment set integer value.
+logical                           :: bval         !< Boolean value.
+logical                           :: vbval        !< Valued-boolean value.
+integer(I8P)                      :: ilist(1:3)   !< Integer list values.
+real(R8P),    allocatable         :: vlistR8P(:)  !< Varying size real list values.
+real(R4P),    allocatable         :: vlistR4P(:)  !< Varying size real list values.
+integer(I8P), allocatable         :: vlistI8P(:)  !< Varying size integer list values.
+integer(I4P), allocatable         :: vlistI4P(:)  !< Varying size integer list values.
+integer(I2P), allocatable         :: vlistI2P(:)  !< Varying size integer list values.
+integer(I1P), allocatable         :: vlistI1P(:)  !< Varying size integer list values.
+logical,      allocatable         :: vlistBool(:) !< Varying size boolean list values.
+character(10),allocatable         :: vlistChar(:) !< Varying size character list values.
+character(99),allocatable         :: garbage(:)   !< Varying size character list for trailing garbage values.
+integer(I4P)                      :: error        !< Error trapping flag.
+integer(I4P)                      :: l            !< Counter.
 !-----------------------------------------------------------------------------------------------------------------------------------
 
 !-----------------------------------------------------------------------------------------------------------------------------------
-! initialize IR_Precision
-call IR_Init
-! print main IR_Precision values
-call IR_Print(unit=stdout)
-
-print "(A)", ''
-print "(A)", 'Testing IR_Precision tools'
-print "(A)", 'Casting real-to-string: '//str(n=1._R8P)
-print "(A)", 'Casting integer-to-string: '//str(n=11_I8P)
-print "(A,"//FR8P//")", 'Casting string-to-real: ',cton(str='2.2d0',knd=1._R8P)
-print "(A,"//FI4P//")", 'Casting integer-to-string: ',cton(str='43',knd=1_I4P)
-print "(A)", 'Casting integer-to-string with zero padding: '//trim(strz(nz_pad=3,n=34_I8P))
-! print "(A)", 'Casting real-to-bit_string: '//bstr(n=1._R4P)
-print "(A)", 'Casting integer-to-bit_string: '//bstr(n=1_I4P)
-! print "(A,"//FR4P//")", 'Casting bit_string-to-real: ',bcton(bstr='00111111100000000000000000000000',knd=1._R4P)
-print "(A,"//FI4P//")", 'Casting bit_string-to-integer: ',bcton(bstr='00000000000000000000000000000001',knd=1_I4P)
-print "(A)", 'Number of digit of 1023: '//str(n=digit(1023_I4P))
+! initializing Command Line Interface
+call cli%init(progname    = 'Test_Driver',                                                 &
+              version     = 'v2.1.5',                                                      &
+              authors     = 'Stefano Zaghi',                                               &
+              license     = 'MIT',                                                         &
+              help        = 'Usage: ',                                                     &
+              description = 'Toy program for testing FLAP',                                &
+              examples    = ["Test_Driver -s 'Hello FLAP'                               ", &
+                             "Test_Driver -s 'Hello FLAP' -i -2 # printing error...     ", &
+                             "Test_Driver -s 'Hello FLAP' -i 3 -ie 1 # printing error...", &
+                             "Test_Driver -s 'Hello FLAP' -i 3 -r 33.d0                 ", &
+                             "Test_Driver -s 'Hello FLAP' --integer_list 10 -3 87       ", &
+                             "Test_Driver -s 'Hello FLAP' --man_file FLAP.1             ", &
+                             "Test_Driver 33.0 -s 'Hello FLAP' -i 5                     ", &
+                             "Test_Driver --string 'Hello FLAP' --boolean               "],&
+              epilog      = new_line('a')//"And that's how to FLAP your life")
+! setting Command Line Argumenst
+call cli%add(switch='--string',switch_ab='-s',help='String input',required=.true.,act='store',error=error)
+if (error/=0) stop
+call cli%add(switch='--integer_ex',switch_ab='-ie',help='Exclusive integer input',required=.false.,act='store',def='-1',error=error)
+if (error/=0) stop
+call cli%add(switch='--integer',switch_ab='-i',help='Integer input with fixed range',required=.false.,act='store',&
+             def='1',choices='1,3,5',exclude='-ie',error=error)
+if (error/=0) stop
+call cli%add(switch='--real',switch_ab='-r',help='Real input',required=.false.,act='store',def='1.0',error=error)
+if (error/=0) stop
+call cli%add(switch='--boolean',switch_ab='-b',help='Boolean input',required=.false.,act='store_true',def='.false.',&
+             error=error)
+if (error/=0) stop
+call cli%add(switch='--boolean_val',switch_ab='-bv',help='Valued boolean input',required=.false., act='store',&
+             def='.true.',error=error)
+if (error/=0) stop
+call cli%add(switch='--integer_list',switch_ab='-il',help='Integer list input',required=.false.,act='store',&
+             nargs='3',def='1 8 32',error=error)
+if (error/=0) stop
+call cli%add(positional=.true.,position=1,help='Positional real input',required=.false.,def='1.0',error=error)
+if (error/=0) stop
+call cli%add(switch='--env',switch_ab='-e',help='Environment input',required=.false.,act='store',def='-1',envvar='FLAP_NUM_INT',&
+             error=error)
+if (error/=0) stop
+call cli%add(switch='--man_file',help='Save manual into man_file',required=.false.,act='store',def='Test_Driver.1',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listR8P',switch_ab='-vlR8P',help='Varying size real R8P list input',required=.false.,act='store',&
+             nargs='*',def='1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listR4P',switch_ab='-vlR4P',help='Varying size real R4P list input',required=.false.,act='store',&
+             nargs='*',def='1.0 2.0 3.0 4.0',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listI8P',switch_ab='-vlI8P',help='Varying size integer I8P list input',required=.false.,act='store',&
+             nargs='*',def='1 2 3 4 5 6 7 8',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listI4P',switch_ab='-vlI4P',help='Varying size integer I4P list input',required=.false.,act='store',&
+             nargs='*',def='1 2 3 4',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listI2P',switch_ab='-vlI2P',help='Varying size integer I2P list input',required=.false.,act='store',&
+             nargs='*',def='1 2',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listI1P',switch_ab='-vlI1P',help='Varying size integer I1P list input',required=.false.,act='store',&
+             nargs='+',def='1',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listBool',switch_ab='-vlBool',help='Varying size boolean list input',required=.false.,act='store',&
+             nargs='*',def='T F T T F',error=error)
+if (error/=0) stop
+call cli%add(switch='--varying_listChar',switch_ab='-vlChar',help='Varying size character list input',required=.false.,act='store',&
+             nargs='*',def='foo bar baz',error=error)
+if (error/=0) stop
+! parsing Command Line Interface
+call cli%parse(error=error)
+if (error/=0) stop
+! using Command Line Interface data to set Test_Driver behaviour
+call cli%get(        switch='-s',      val=sval,      error=error) ; if (error/=0) stop
+call cli%get(        switch='-r',      val=rval,      error=error) ; if (error/=0) stop
+call cli%get(        switch='-i',      val=ival,      error=error) ; if (error/=0) stop
+call cli%get(        switch='-ie',     val=ieval,     error=error) ; if (error/=0) stop
+call cli%get(        switch='-b',      val=bval,      error=error) ; if (error/=0) stop
+call cli%get(        switch='-bv',     val=vbval,     error=error) ; if (error/=0) stop
+call cli%get(        switch='-il',     val=ilist,     error=error) ; if (error/=0) stop
+call cli%get(        switch='-e',      val=envi,      error=error) ; if (error/=0) stop
+call cli%get(        position=1_I4P,   val=prval,     error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlR8P',  val=vlistR8P,  error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlR4P',  val=vlistR4P,  error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlI8P',  val=vlistI8P,  error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlI4P',  val=vlistI4P,  error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlI2P',  val=vlistI2P,  error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlI1P',  val=vlistI1P,  error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlBool', val=vlistBool, error=error) ; if (error/=0) stop
+call cli%get_varying(switch='-vlChar', val=vlistChar, error=error) ; if (error/=0) stop
+call cli%get_varying(switch='--',      val=garbage,   error=error) ; if (error/=0) stop
+print '(A)'   ,'Test_Driver has been called with the following arguments values:'
+print '(A)'   ,'String              input = '//trim(adjustl(sval))
+print '(A)'   ,'Real                input = '//str(n=rval)
+print '(A)'   ,'Integer             input = '//str(n=ival)
+print '(A)'   ,'Exclusive   integer input = '//str(n=ieval)
+print '(A)'   ,'Environment integer input = '//str(n=envi)
+print '(A,L1)','Boolean             input = ',bval
+print '(A,L1)','Valued boolean      input = ',vbval
+print '(A)'   ,'Positional real     input = '//str(n=prval)
+print '(A)'   ,'Integer list inputs:'
+do l=1, 3
+  print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//trim(str(n=ilist(l)))
+enddo
+if (allocated(vlistR8P)) then
+  print '(A)'   ,'Varying size real R8P list inputs:'
+  do l=1, size(vlistR8P)
+    print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//trim(str(n=vlistR8P(l)))
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size real R8P list!'
+endif
+if (allocated(vlistR4P)) then
+  print '(A)'   ,'Varying size real R4P list inputs:'
+  do l=1, size(vlistR4P)
+    print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//trim(str(n=vlistR4P(l)))
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size real R4P list!'
+endif
+if (allocated(vlistI8P)) then
+  print '(A)'   ,'Varying size integer I8P list inputs:'
+  do l=1, size(vlistI8P)
+    print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//trim(str(n=vlistI8P(l)))
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size integer I8P list!'
+endif
+if (allocated(vlistI4P)) then
+  print '(A)'   ,'Varying size integer I4P list inputs:'
+  do l=1, size(vlistI4P)
+    print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//trim(str(n=vlistI4P(l)))
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size integer I4P list!'
+endif
+if (allocated(vlistI2P)) then
+  print '(A)'   ,'Varying size integer I2P list inputs:'
+  do l=1, size(vlistI2P)
+    print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//trim(str(n=vlistI2P(l)))
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size integer I2P list!'
+endif
+if (allocated(vlistI1P)) then
+  print '(A)'   ,'Varying size integer I1P list inputs:'
+  do l=1, size(vlistI1P)
+    print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//trim(str(n=vlistI1P(l)))
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size integer I1P list!'
+endif
+if (allocated(vlistBool)) then
+  print '(A)'   ,'Varying size boolean list inputs:'
+  do l=1, size(vlistBool)
+    print '(A,L1)' ,'  Input('//trim(str(.true.,l))//') = ',vlistBool(l)
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size boolean list!'
+endif
+if (allocated(vlistChar)) then
+  print '(A)'   ,'Varying size character list inputs:'
+  do l=1, size(vlistChar)
+    print '(A)' ,'  Input('//trim(str(.true.,l))//') = '//vlistChar(l)
+  enddo
+else
+  print '(A)'   ,'Problems occuour with varying size character list!'
+endif
+if (allocated(garbage)) then
+  print '(A)'   ,'You have used implicit "--" option for collecting list of "trailing garbage" values that are:'
+  do l=1, size(garbage)
+    print '(A)' ,'  Garbage('//trim(str(.true.,l))//') = '//garbage(l)
+  enddo
+endif
+if (cli%passed(switch='--man_file')) then
+  call cli%get(switch='--man_file',val=sval,error=error) ; if (error/=0) stop
+  print '(A)','Saving man page'
+  call cli%save_man_page(error=error,man_file=trim(adjustl(sval)))
+endif
 stop
 !-----------------------------------------------------------------------------------------------------------------------------------
 endprogram Test_Driver
